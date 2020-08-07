0752f76bbe394d0e6c094225f1174d457f143967
Salma Akter <salmaakter@Salmas-MacBook-Pro.local>
2020-08-07 15:30:14 +0600

    first ever commit

---
+++ true-false.js
@@ -0,0 +1,15 @@
+//Falsy:
+// 0
+// ""
+// udenfined
+// null
+// NaN
+//Truthy
+//'0' ' ' []
+let name = 0;
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
